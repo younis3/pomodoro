6550e6a4d57987fd85e48af4e2efd54f39b9f616
younis3 <younis3dev@gmail.com>
2022-09-13 18:25:39 +0300

    history table structure and getting data

--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import styled from "styled-components";
+import HistoryTable from "../components/HistoryTable";
+import { auth } from "../firebase";
+import { useEffect, useState } from "react";
 
 const StatsPage = () => {
-  return <div></div>;
+  const [userObj, setUserObj] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        auth.currentUser = user;
+        if (auth.currentUser) {
+          //user signed in
+          setUserObj(auth.currentUser);
+        }
+      }
+    });
+  });
+
+  return (
+    <div>
+      {userObj && (
+        <StyledHistoryTab>
+          <h3 className="tableTitle">History Table</h3>
+          <HistoryTable user={userObj} />
+        </StyledHistoryTab>
+      )}
+
+      <StyledStatsTab></StyledStatsTab>
+    </div>
+  );
 };
 
 export default StatsPage;
+//
+//
+//
+//
+//
+/****************** styles ******************/
+const StyledHistoryTab = styled.div`
+  .tableTitle {
+    padding-top: 10vh;
+    margin-bottom: 3vh;
+    color: #fff;
+  }
+`;
+
+const StyledStatsTab = styled.div``;
